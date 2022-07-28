798389a3a3b5026ceacaf465a086c169eeebd0e8
yourilwill <yourilwill.n.h@gmail.com>
2022-07-29 05:35:43 +0900

    auto commit at 2022/07/29 05:35:43

---
+++ git_push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COMMIT_DATE=$(date "+%Y/%m/%d %H:%M:%S")
+GIT_DIRECTORY=/home/vagrant/test/test_git
+GIT_COMMAND="git add -A ; git commit -m 'auto commit at $COMMIT_DATE' ; git push"
+
+cd $GIT_DIRECTORY
+
+eval $GIT_COMMAND
